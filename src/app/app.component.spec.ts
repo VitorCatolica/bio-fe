7e87d923866eb6c2a6dd87aa13bdb1f2f2a7f830
Vitor <vitor.leopoldo@selbetti.com.br>
2026-02-08 10:46:07 -0300

    maintance

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'personal-fe'`, () => {
+  it(`should have as title 'bio-fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-fe');
+    expect(app.title).toEqual('bio-fe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('personal-fe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bio-fe app is running!');
   });
 });
